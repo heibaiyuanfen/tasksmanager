f6bed8d6677d566213768a0769852219d20dd67d
heibaiyuanfen <heibaiyuanfen@gmail.com>
2023-11-27 11:09:00 +0800

    完成任务号和进程号的显示

---
+++ WpfApp1/WpfApp1/Enity/taskinfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class taskinfo
+    {
+        public string TaskNumber { get; set; }
+        public int ProcessId { get; set; }
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+
+        private string GetTaskNumberFromProcessName(string processName)
+        {
+            // 这是一个示例方法，你可以根据实际情况提取任务号
+            // 这里我们简单地返回进程名称作为任务号
+            return processName;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+
+            // 清空ListBox以便显示新的任务号和进程号
+            taskListBox.Items.Clear();
+
+            // 获取所有正在运行的进程
+            var processes = Process.GetProcesses();
+
+            // 遍历进程并提取任务号和进程号
+            foreach (var process in processes)
+            {
+                try
+                {
+                    string processName = process.ProcessName;
+                    int processId = process.Id;
+
+                    string taskNumber = GetTaskNumberFromProcessName(processName);
+
+                    if (!string.IsNullOrEmpty(taskNumber))
+                    {
+                        // 创建一个TaskInfo对象来存储任务号和进程号
+                        taskinfo taskInfo = new taskinfo
+                        {
+                            TaskNumber = taskNumber,
+                            ProcessId = processId
+                        };
+
+                        taskListBox.Items.Add(taskInfo);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // 处理异常，例如无法访问某些进程的权限问题
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
